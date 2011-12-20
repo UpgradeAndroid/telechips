75c6d9d9d957369bea174a906e893480a8bc4011
한성민 (SM Han) <sungminhan@telechips.com>
2011-12-20 18:19:58 +0900

    update remocon driver for tcc892x
    Issue       : update remocon driver for tcc892x
    Platform    : ICS
    Repository  :
    Description : update remocon driver for tcc892x

--- drivers/input/misc/tca_remocon.c
+++ drivers/input/misc/tca_remocon.c
@@ -28,26 +28,33 @@ void    RemoconCommandOpen (void)
 {
 	PREMOTECON      pRcu = (volatile PREMOTECON)tcc_p2v(HwREMOTE_BASE);
 
-	BITCLR(pRcu->CMD, Hw0);
-	BITSET(pRcu->CMD, Hw1|Hw2|(20*Hw3)|Hw12|Hw13|Hw14);
+	#if defined(CONFIG_ARCH_TCC892X)
+		BITCLR(pRcu->CMD.nREG, Hw0);
+		BITSET(pRcu->CMD.nREG, Hw1|Hw2|(20*Hw3)|Hw12|Hw13|Hw14);
+	#else
+		BITCLR(pRcu->CMD, Hw0);
+		BITSET(pRcu->CMD, Hw1|Hw2|(20*Hw3)|Hw12|Hw13|Hw14);
+	#endif
 }
 
 void    RemoconCommandReset (void)
 {
 	PREMOTECON      pRcu = (volatile PREMOTECON)tcc_p2v(HwREMOTE_BASE);
 	
-	if(machine_is_tcc8900() || machine_is_tcc9200() || machine_is_tcc9201() || machine_is_tcc9200s()) {
-		pRcu->CMD = Hw1|Hw2|(14*Hw3)|Hw13|Hw14;
-	}
-	else {
-#ifdef USE_PBUS_CLOCK
-		pRcu->CMD = Hw1|Hw2|(14*Hw3)|Hw13|Hw14;
-#else
-		BITCLR(pRcu->CMD, Hw0);
-		BITCLR(pRcu->CMD, Hw12);
-		BITSET(pRcu->CMD, Hw1|Hw2|(20*Hw3)|Hw13|Hw14);
-#endif
-	}
+	#if defined(CONFIG_ARCH_TCC892X)
+		BITCLR(pRcu->CMD.nREG, Hw0);
+		BITCLR(pRcu->CMD.nREG, Hw12);
+		BITSET(pRcu->CMD.nREG, Hw1|Hw2|(20*Hw3)|Hw13|Hw14);
+	#else
+		if(machine_is_tcc8900() || machine_is_tcc9200() || machine_is_tcc9201() || machine_is_tcc9200s()) {
+			pRcu->CMD = Hw1|Hw2|(14*Hw3)|Hw13|Hw14;
+		}
+		else {
+			BITCLR(pRcu->CMD, Hw0);
+			BITCLR(pRcu->CMD, Hw12);
+			BITSET(pRcu->CMD, Hw1|Hw2|(20*Hw3)|Hw13|Hw14);
+		}
+	#endif
 }
 
 //======================================================
@@ -57,20 +64,31 @@ void    RemoconConfigure (void)
 {
 	PREMOTECON      pRcu = (volatile PREMOTECON)tcc_p2v(HwREMOTE_BASE);
 
-	if(machine_is_tcc8900() || machine_is_tcc9200() || machine_is_tcc9201() || machine_is_tcc9200s()) {
-		BITSET(pRcu->INPOL, Hw1);             // polarity enable
-	}
-	else {
-#ifdef USE_PBUS_CLOCK
-		BITCLR(pRcu->INPOL, Hw8);
-#else
-		BITSET(pRcu->INPOL, Hw8);
-#endif
-		BITSET(pRcu->INPOL, Hw0|Hw9|Hw10|Hw11);
-		//BITCLR(pRcu->INPOL, (Hw9|Hw10|Hw11));
+	#if defined(CONFIG_ARCH_TCC892X)
+		#ifdef USE_PBUS_CLOCK
+			BITCLR(pRcu->INPOL.nREG, Hw8);
+		#else
+			BITSET(pRcu->INPOL.nREG, Hw8);
+		#endif
+			BITSET(pRcu->INPOL.nREG, Hw0|Hw9|Hw10|Hw11);
 
-		tcc_remocon_set_io();
-	}
+			tcc_remocon_set_io();
+	#else
+		if(machine_is_tcc8900() || machine_is_tcc9200() || machine_is_tcc9201() || machine_is_tcc9200s()) {
+			BITSET(pRcu->INPOL, Hw1);             // polarity enable
+		}
+		else {
+			#ifdef USE_PBUS_CLOCK
+				BITCLR(pRcu->INPOL, Hw8);
+			#else
+				BITSET(pRcu->INPOL, Hw8);
+			#endif
+			BITSET(pRcu->INPOL, Hw0|Hw9|Hw10|Hw11);
+			//BITCLR(pRcu->INPOL, (Hw9|Hw10|Hw11));
+
+			tcc_remocon_set_io();
+		}
+	#endif
 }
 
 //======================================================
@@ -80,7 +98,11 @@ void    RemoconStatus (void)
 {
 	PREMOTECON      pRcu = (volatile PREMOTECON)tcc_p2v(HwREMOTE_BASE);
 
+#if defined(CONFIG_ARCH_TCC892X)
+	BITCSET(pRcu->STA.nREG, 0x0, Hw12);
+#else
 	BITCSET(pRcu->STA, 0x0, Hw12);
+#endif
 }
 
 //======================================================
@@ -91,18 +113,28 @@ void    RemoconDivide (void)
 {
 	PREMOTECON      pRcu = (volatile PREMOTECON)tcc_p2v(HwREMOTE_BASE);
 
-	if(machine_is_tcc8900() || machine_is_tcc9200() || machine_is_tcc9201() || machine_is_tcc9200s()) {
-		pRcu->CLKDIV = ((500*24)*Hw14)|(1*Hw0);
-	}
-	else{
-#ifdef USE_PBUS_CLOCK
-		pRcu->CLKDIV = ((500*36)*Hw14)|(1*Hw0);
-#else
-		pRcu->CLKDIV = (1*Hw0);
-#endif
-	}
+	#if defined(CONFIG_ARCH_TCC892X)
+		#ifdef USE_PBUS_CLOCK
+			pRcu->CLKDIV.nREG = ((500*10)*Hw14)|(1*Hw0);
+		#else
+			pRcu->CLKDIV.nREG = (1*Hw0);
+		#endif
 
-	pRcu->CLKDIV = BITCSET(pRcu->CLKDIV, 0x000000FF, 100);	// Set END_CNT as 100
+		pRcu->CLKDIV.nREG = BITCSET(pRcu->CLKDIV.nREG, 0x000000FF, 0xFE);	// Set END_CNT as 0xFE
+	#else
+		if(machine_is_tcc8900() || machine_is_tcc9200() || machine_is_tcc9201() || machine_is_tcc9200s()) {
+			pRcu->CLKDIV = ((500*24)*Hw14)|(1*Hw0);
+		}
+		else{
+			#ifdef USE_PBUS_CLOCK
+				pRcu->CLKDIV = ((500*30)*Hw14)|(1*Hw0);
+			#else
+				pRcu->CLKDIV = (1*Hw0);
+			#endif
+		}
+
+		pRcu->CLKDIV = BITCSET(pRcu->CLKDIV, 0x000000FF, 100);	// Set END_CNT as 100
+	#endif
 }
 
 //======================================================
@@ -112,8 +144,13 @@ void    DisableRemocon (void)
 {
 	PREMOTECON      pRcu = (volatile PREMOTECON)tcc_p2v(HwREMOTE_BASE);
 
+#if defined(CONFIG_ARCH_TCC892X)
+	BITSET(pRcu->CMD.nREG, Hw0);
+	BITCLR(pRcu->CMD.nREG, Hw1|Hw2|Hw13|Hw14);
+#else
 	BITSET(pRcu->CMD, Hw0);
 	BITCLR(pRcu->CMD, Hw1|Hw2|Hw13|Hw14);
+#endif
 }
 
 //======================================================
@@ -127,13 +164,26 @@ void    RemoconInit (void)
 	RemoconIrqClear();
 	RemoconIntClear();
 
+#if defined(CONFIG_ARCH_TCC892X)
+	pRcu->CLKDIV.nREG = BITCSET(pRcu->CLKDIV.nREG, 0x000000FF, 0xFE);	// Set END_CNT as 0xFE
+#else
 	pRcu->CLKDIV = BITCSET(pRcu->CLKDIV, 0x000000FF, 100);	// Set END_CNT as 100
+#endif
 }
 
 void    RemoconIntClear ()
 {
 	PREMOTECON      pRcu = (volatile PREMOTECON)tcc_p2v(HwREMOTE_BASE);
 
+#if defined(CONFIG_ARCH_TCC892X)
+	BITCLR(pRcu->CMD.nREG, Hw2);
+	BITSET(pRcu->CMD.nREG, Hw0);
+
+	udelay(120);
+
+	BITSET(pRcu->CMD.nREG, Hw2);
+	BITCLR(pRcu->CMD.nREG, Hw0);
+#else
 	BITCLR(pRcu->CMD, Hw2);
 	BITSET(pRcu->CMD, Hw0);
 
@@ -141,6 +191,7 @@ void    RemoconIntClear ()
 
 	BITSET(pRcu->CMD, Hw2);
 	BITCLR(pRcu->CMD, Hw0);
+#endif
 }
 
 void    RemoconIntWait ()
@@ -148,6 +199,15 @@ void    RemoconIntWait ()
 	PPIC pPic = (volatile PPIC)tcc_p2v(HwPIC_BASE);
 	uint rx_data;
 
+#if defined(CONFIG_ARCH_TCC892X)
+	while(1)
+	{
+		rx_data = pPic->STS1.nREG;
+		if((rx_data&0x400) == 0x400)
+			break;
+	}
+	pPic->CLR1.nREG = 0x400;     //RMT bit set to 1 which means rmt interrupt clear      
+#else
 	while(1)
 	{
 		rx_data = pPic->STS1;
@@ -155,6 +215,7 @@ void    RemoconIntWait ()
 			break;
 	}
 	pPic->CLR1 = 0x400;     //RMT bit set to 1 which means rmt interrupt clear      
+#endif
 }
 
 void    RemoconIrqClear ()
@@ -162,7 +223,12 @@ void    RemoconIrqClear ()
 	uint idx;
 	PPIC pPic = (volatile PPIC)tcc_p2v(HwPIC_BASE);
 
+#if defined(CONFIG_ARCH_TCC892X)
+	for(idx=1; idx<3000; idx ++)
+		pPic->CLR1.nREG = 0x400;                     // Clear VIC Interrupt Clear Register 
+#else
 	for(idx=1; idx<3000; idx ++)
 		pPic->CLR1 = 0x400;                     // Clear VIC Interrupt Clear Register 
+#endif
 }
 
