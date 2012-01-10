d484ed49ef05b6a283b1f88b69fdc3d7834a80f3
YH Choi <see21@telechips.com>
2012-01-10 14:43:12 +0900

    Issue : compile error fix, some codes are missed from previous commit
    Platform : tcc88xx
    Repository : kernel

--- arch/arm/mach-tcc88xx/include/mach/tca_tsif.h
+++ arch/arm/mach-tcc88xx/include/mach/tca_tsif.h
@@ -148,5 +148,6 @@ struct tcc_tsif_handle {
 extern int tca_tsif_init(struct tcc_tsif_handle *h, volatile struct tcc_tsif_regs *regs, 
 		dma_alloc_f tea_dma_alloc, dma_free_f tea_dma_free, int dma_size, int tsif_ch, int dma_controller, int dma_ch, int port);
 extern void tca_tsif_clean(struct tcc_tsif_handle *h);
+extern int tca_tsif_register_pids(struct tcc_tsif_handle *h, unsigned int *pids, unsigned int count);
 
 #endif /*__TCC_TSIF_MODULE_HWSET_H__*/
