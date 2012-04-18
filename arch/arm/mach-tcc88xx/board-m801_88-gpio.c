20e89586151af385c13893f2e2879054594fcd10
Sangwon Lee (B070586) <leesw@telechips.com>
2012-04-18 11:32:14 +0900

    tcc880x: re-arrange the i2c devices

--- arch/arm/mach-tcc88xx/board-m801_88-gpio.c
+++ arch/arm/mach-tcc88xx/board-m801_88-gpio.c
@@ -21,20 +21,14 @@
 #include "board-m801_88.h"
 #include <asm/mach-types.h>
 
-struct board_gpio_irq_config m801_88_gpio_irqs[] = {
+static struct board_gpio_irq_config m801_88_gpio_irqs[] = {
 	{ -1, -1 },
-};
-
-/* I2C core0 channel0 devices */
-static struct i2c_board_info __initdata i2c_devices0[] = {
 };
 
 void __init m801_88_init_gpio(void)
 {
 	if (!machine_is_m801_88())
 		return;
-
-	i2c_register_board_info(0, i2c_devices0, ARRAY_SIZE(i2c_devices0));
 
 	board_gpio_irqs = m801_88_gpio_irqs;
 	printk(KERN_INFO "M801_88 GPIO initialized\n");
--- arch/arm/mach-tcc88xx/board-m803-gpio.c
+++ arch/arm/mach-tcc88xx/board-m803-gpio.c
@@ -21,20 +21,14 @@
 #include "board-m803.h"
 #include <asm/mach-types.h>
 
-struct board_gpio_irq_config m803_gpio_irqs[] = {
+static struct board_gpio_irq_config m803_gpio_irqs[] = {
 	{ -1, -1 },
-};
-
-/* I2C core0 channel0 devices */
-static struct i2c_board_info __initdata i2c_devices0[] = {
 };
 
 void __init m803_init_gpio(void)
 {
 	if (!machine_is_m803())
 		return;
-
-	i2c_register_board_info(0, i2c_devices0, ARRAY_SIZE(i2c_devices0));
 
 	board_gpio_irqs = m803_gpio_irqs;
 	printk(KERN_INFO "M803 GPIO initialized\n");
