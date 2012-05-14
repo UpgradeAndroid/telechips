7e9032a93f628407cc946cbbaef868f40c0d843a
Sangwon Lee (B070586) <leesw@telechips.com>
2012-05-14 17:48:34 +0900

    m805_8925: remove 25mA core voltage offset on M805_8925

--- arch/arm/mach-tcc892x/cpufreq.c
+++ arch/arm/mach-tcc892x/cpufreq.c
@@ -61,17 +61,9 @@ struct tcc_voltage_table_t {
 };
 
 #if defined(CONFIG_TCC_CORE_VOLTAGE_OFFSET) && defined(CONFIG_REGULATOR)
-	#if defined(CONFIG_M805S_8925_0XX)
-	#define CORE_VOLTAGE_OFFSET	((CONFIG_TCC_CORE_VOLTAGE_OFFSET + 25) * 1000)
-	#else
 	#define CORE_VOLTAGE_OFFSET	(CONFIG_TCC_CORE_VOLTAGE_OFFSET * 1000)
-	#endif
 #else
-	#if defined(CONFIG_M805S_8925_0XX)
-	#define CORE_VOLTAGE_OFFSET	(25 * 1000)
-	#else
 	#define CORE_VOLTAGE_OFFSET	(0)
-	#endif
 #endif
 
 #if defined(CONFIG_TCC892X_NN)
