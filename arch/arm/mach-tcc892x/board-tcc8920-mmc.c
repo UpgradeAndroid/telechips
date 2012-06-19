a6d8e50d6be53afb6e7fe9d228a592ae80a0e5c7
hjbae <B060269@C2-L-Dev2.(none)>
2012-06-19 14:57:34 +0900

    Issue : Fixed the problem - suspend/resume lock-up issue.
    Platform : all
    Repository : tcc.git
    Description : Add the msleep.

--- arch/arm/mach-tcc892x/board-tcc8920-mmc.c
+++ arch/arm/mach-tcc892x/board-tcc8920-mmc.c
@@ -185,9 +185,13 @@ int tcc8920_mmc_init(struct device *dev, int id)
 	{
 		gpio_request(mmc_port->pwr, "WiFi_Power");
 		gpio_direction_output(mmc_port->pwr, 0);
-		msleep(100);
+		msleep(300);	//for EVM - suspend.resume issue (100->300)
 		gpio_direction_output(mmc_port->pwr, 1);
 	}
+	//Start : for EVM - suspend.resume issue
+	if((system_rev >= 0x1000)&&(system_rev <= 0x1008))
+		msleep(300);
+	//End
 
 	#if defined(CONFIG_BCM4330_SUB_BOARD)
 	gpio_request(GPIO_SDWF_RST, "bcm4330_on");
