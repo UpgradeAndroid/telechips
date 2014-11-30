39f25061820e3e5cfe6265b79c05371ea8500fe7
Ithamar R. Adema <ithamar@upgrade-android.com>
2014-11-30 14:17:11 +0100

    tca_keypad cleanup.

---
+++ include/linux/input/tcc_keypad.h
@@ -0,0 +1,18 @@
+#ifndef TCC_KEYPAD_H
+#define TCC_KEYPAD_H
+
+struct tcc_key_info {
+	// virtual key code
+	int code;
+	// start and end ADC values
+	unsigned int start;
+	unsigned int end;
+};
+
+struct tcc_keypad_pdata {
+	int adc_channel;
+	struct tcc_key_info *keys;
+	int num_keys;
+};
+
+#endif // TCC_KEYPAD_H
